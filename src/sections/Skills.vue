6199b013070b68139f7db773433a7ef7189b4c2f
Prima Putra Agusantara <primatara42@gmail.com>
2026-05-10 17:08:04 +0700

    feat: add skills section

---
+++ src/sections/Skills.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+<template>
+  <section class="bg-background px-6 py-10">
+    <h1 class="text-text items-center text-center font-bold text-3xl">
+      Skills
+    </h1>
+
+    <p class="text-text text-base text-center font-normal my-6">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe dolor
+      accusamus dignissimos accusantium omnis recusandae excepturi autem
+      voluptates iure itaque! Harum odio dolorum expedita tenetur, quisquam
+      nobis! Sit, fugiat deserunt!
+    </p>
+  </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
 import Navbar from "../components/Navbar.vue";
 import About from "../sections/About.vue";
 import Experience from "../sections/Experience.vue";
+import Skills from "../sections/Skills.vue";
 </script>
 <template>
   <Navbar />
   <About />
   <Experience />
+  <Skills />
 </template>
