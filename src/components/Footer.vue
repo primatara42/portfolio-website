607a325b5e0001e5d866bcef2d4a8393ee2e38be
Prima Putra Agusantara <primatara42@gmail.com>
2026-06-07 22:07:55 +0700

    feat: add social media links to footer icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,17 +10,35 @@ import whatsapp_icon from "../assets/icons/whatsapp-icon.svg";
         <div
           class="w-12 h-12 bg-gray-800 px-2 py-2 rounded-full hover:bg-text hover:scale-110 transition duration-100 hover:cursor-pointer"
         >
-          <div class="w-full"><img :src="linkedin_icon" alt="" /></div>
+          <div class="w-full">
+            <a
+              href="https://www.linkedin.com/in/prima-putra-agusantara-014140176"
+              target="_blank"
+              ><img :src="linkedin_icon" alt=""
+            /></a>
+          </div>
         </div>
         <div
           class="w-12 h-12 bg-gray-800 px-2 py-2 rounded-full hover:bg-text hover:scale-110 transition duration-100 hover:cursor-pointer"
         >
-          <div class="w-full"><img :src="instagram_icon" alt="" /></div>
+          <div class="w-full">
+            <a
+              href="https://www.instagram.com/prima_putraagusantara/"
+              target="_blank"
+              ><img :src="instagram_icon" alt=""
+            /></a>
+          </div>
         </div>
         <div
           class="w-12 h-12 bg-gray-800 px-2 py-2 rounded-full hover:bg-text hover:scale-110 transition duration-100 hover:cursor-pointer"
         >
-          <div class="w-full"><img :src="whatsapp_icon" alt="" /></div>
+          <div class="w-full">
+            <a
+              href="https://api.whatsapp.com/send/?phone=6281958305632&text&type=phone_number&app_absent=0"
+              target="_blank"
+              ><img :src="whatsapp_icon" alt="" />
+            </a>
+          </div>
         </div>
       </div>
       <div>
